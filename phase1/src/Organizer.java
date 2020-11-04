795e86863539bf873fdffdd1ee73244352d58098
jordanbotines <jordanbotines@gmail.com>
2020-11-04 00:04:45 -0500

    Jordan - Changing Organizer class got rid of organizingEvent

--- phase1/src/Organizer.java
+++ phase1/src/Organizer.java
@@ -3,23 +3,19 @@ import java.util.List;
 
 public class Organizer extends User {
     private List<String> attendingEvents;
-    private String organizingEvent;
+    private List<String> organizingEvents;
 
-    public Organizer(String name, String address, String email, String userName, String password, String organizingEvent) {
+    public Organizer(String name, String address, String email, String userName, String password) {
         super(name, address, email, userName, password);
         this.attendingEvents = new ArrayList<String>();
-        this.organizingEvent = organizingEvent; // WHAT IS THIS?
+        this.organizingEvents = new ArrayList<String>();
     }
 
-    public String getOrganizingEvent(){
-        return this.organizingEvent;
+    public List<String> getOrganizingEvents(){
+        return this.organizingEvents;
     }
     public List<String> getAttendingEvents(){
         return this.attendingEvents;
-    }
-
-    public void setOrganizingEvent(String eventName){
-        this.organizingEvent = eventName;
     }
     public void signUpForEvent(Event event){
         this.attendingEvents.add(event.getName());
