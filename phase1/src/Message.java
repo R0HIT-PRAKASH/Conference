ef6d03a3d30627e9b882d6adf85b71afe7ab1d01
joshuaemerson <joshua.emerson@mail.utoronto.ca>
2020-11-05 16:28:47 -0500

    Added the setter and getter methods for the Message class

--- phase1/src/Message.java
+++ phase1/src/Message.java
@@ -10,4 +10,19 @@ public Message(String message, String senderUsername, String recipientUsername){
     this.recipientUsername = recipientUsername;
 }
 
+// Getters
+
+public String getMessage(){ return message; }
+
+public String getRecipient(){ return recipientUsername; }
+
+public String getSender(){ return senderUsername; }
+
+// Setters
+
+public void setMessage(String message){ this.message = message; }
+
+public void setRecipient(String recipientUsername){ this.recipientUsername = recipientUsername; }
+
+public void setSender(String senderUsername){ this.senderUsername = senderUsername; }
 }
