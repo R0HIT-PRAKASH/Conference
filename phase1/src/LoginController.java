420c4448709b52d2c6cb10a73d3ce8c5f62f8c97
Khushkaran Rajput <k.rajput@mail.utoronto.ca>
2020-11-05 17:11:40 -0500

    fixed a mistake in LoginController

--- phase1/src/LoginController.java
+++ phase1/src/LoginController.java
@@ -3,16 +3,18 @@ import java.util.Scanner;
 public class LoginController extends MainController{
     private Scanner scan = new Scanner(System.in);
 
-    public void login(){
-
+    //this is the only method that is actually called
+    public String login(){
+        //gets input from the User- i.e. their username and password, and returns the User who was able to login
+        String username = scan.nextLine();
+        String password = scan.nextLine();
     }
-    public User logIn(){
 
-    }
     private boolean checkLoginInfo(String username, String password){
-
+        //checks if the username is valid and if the password matches the username,
+        //using the checkCredentials method in UserManager and returns if it is valid
     }
     private User getUserInfo(String username){
-
+        //returns the User with this username (use getUser in UserManager)
     }
 }
