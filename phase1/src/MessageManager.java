501d505f767ffa2508ce24f097dbb92f04e7380f
Elijah Bishun <elijah.bishun@mail.utoronto.ca>
2020-11-05 15:45:59 -0500

    Created the Message class and its constructor, along with updating the MessageManager class.

--- phase1/src/MessageManager.java
+++ phase1/src/MessageManager.java
@@ -1,2 +1,23 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class MessageManager {
+protected HashMap<String, List<Message>> allUserMessages;
+// How do we "store" this map..
+
+public MessageManager(){
+    this.allUserMessages =  new HashMap<String, List<Message>>();
+}
+
+public boolean createNewMessage(String message, String senderUsername, String recipientUsername){
+    Message newMessage = new Message(message, senderUsername, recipientUsername);
+    return true;
+}
+
+public List<Message> viewMessages(String username){
+    return allUserMessages.get(username);
+}
+
+
 }
