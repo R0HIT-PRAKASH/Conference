2eeb0110260dbfab0194340371088c63aa94320f
Khushkaran Rajput <k.rajput@mail.utoronto.ca>
2020-11-05 14:16:43 -0500

    adding the methods (no content) to LoginController

--- phase1/src/LoginController.java
+++ phase1/src/LoginController.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 public class LoginController extends MainController{
+    private Scanner scan = new Scanner(System.in);
 
     public void login(){
+
+    }
+    public User logIn(){
+
+    }
+    private boolean checkLoginInfo(String username, String password){
+
+    }
+    private User getUserInfo(String username){
 
     }
 }
