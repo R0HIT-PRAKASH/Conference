e0ea0beab89969fe42386aece594fbcdc440f453
Khushkaran Rajput <k.rajput@mail.utoronto.ca>
2020-11-07 15:34:41 -0500

    completed LoginController

--- phase1/src/LoginController.java
+++ phase1/src/LoginController.java
@@ -3,18 +3,26 @@ import java.util.Scanner;
 public class LoginController extends MainController{
     private Scanner scan = new Scanner(System.in);
 
-    //this is the only method that is actually called
     public String login(){
-        //gets input from the User- i.e. their username and password, and returns the User who was able to login
         String username = scan.nextLine();
         String password = scan.nextLine();
+        if(this.checkLoginInfo(username, password)){
+            return username;
+        }
+        else{
+            return null;
+        }
     }
 
     private boolean checkLoginInfo(String username, String password){
-        //checks if the username is valid and if the password matches the username,
-        //using the checkCredentials method in UserManager and returns if it is valid
+        username_valid = this.userManager.checkCredentials(username);
+        password_valid = false;
+        if(username_valid){
+            password_valid = (this.getUserInfo(username).getPassword() == password);
+        }
+        return password_valid;
     }
     private User getUserInfo(String username){
-        //returns the User with this username (use getUser in UserManager)
+        return this.userManager.getUser(username);
     }
 }
