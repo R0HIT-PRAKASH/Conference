54df289153502d4be0f61cef0d04d400e4bc67a5
1moonala <junseomoon9@gmail.com>
2020-11-08 15:32:55 -0500

    Added Javadoc comments to all methods.

--- phase1/src/EventManager.java
+++ phase1/src/EventManager.java
@@ -6,21 +6,51 @@ import java.util.Map;
 
 public class EventManager {
 
+    /**
+     * The EventManager class is responsible for handling
+     * event-related actions. events is a map that stores
+     * the names of events along with its associated event
+     * object. rooms is a list of all rooms.
+     *
+     */
+
     private Map<String, Event> events;
     private List<Room> rooms;
 
+    /**
+     * Constructs a new EventManager with an empty map of events
+     * and an empty list of rooms.
+     */
     public EventManager(){
         events = new HashMap<String, Event>();
         rooms =  new ArrayList<Room>();
     }
 
 
+    /**
+     * Creates a new event object.
+     * @param name Refers to the name of the event.
+     * @param speakerName Refers to the name of the speaker of this event.
+     * @param time Refers to the starting time of the event.
+     * @param duration Refers to the duration of this event.
+     * @param roomNumber Refers to the room number of this event
+     * @return Returns the created event.
+     */
     public Event createNewEvent(String name, String speakerName, LocalDateTime time, int duration, int roomNumber){
         Event event = new Event(name, speakerName, time, duration, roomNumber);
         return event;
     }
 
 
+    /**
+     * Adds an event to the events map.
+     * @param name Refers to the name of the event.
+     * @param speakerName Refers to the name of the speaker of this event.
+     * @param time Refers to the starting time of the event.
+     * @param duration Refers to the duration of this event.
+     * @param roomNumber Refers to the room number of this event.
+     * @return Returns true if the event is successfully added. Otherwise, it returns false.
+     */
     public boolean addEvent(String name, String speakerName, LocalDateTime time, int duration, int roomNumber){
         Event event = createNewEvent(name, speakerName, time, duration, roomNumber);
         for (String i: events.keySet()){
@@ -42,11 +72,25 @@ public class EventManager {
 
     }
 
+    /**
+     * Creates a new Speaker user.
+     * @param name Refers to the name of the speaker.
+     * @param address Refers to the address of the speaker.
+     * @param email Refers to the email of the speaker.
+     * @param username Refers to the username of the speaker.
+     * @param password Refers to the password of the speaker.
+     * @return Returns the created speaker.
+     */
     public Speaker createNewSpeaker(String name, String address, String email, String username, String password){
         return new Speaker(name, address, email, username, password);
     }
 
 
+    /**
+     * Finds and gets the specified event from the map of events.
+     * @param eventName Refers the name of the event.
+     * @return Returns the specified event.
+     */
     public Event getEvent(String eventName){
 
         for (String i : events.keySet()){
@@ -60,10 +104,18 @@ public class EventManager {
 
     }
 
+    /**
+     * @return Returns map of events.
+     */
     public Map<String, Event> getAllEvents(){
         return events;
     }
 
+    /**
+     * Checks if the specified event is full.
+     * @param eventName Refers to the name of the event.
+     * @return Returns true if event is full. Otherwise, returns false.
+     */
     public boolean checkEventFull(String eventName){
 
         Event event = this.getEvent(eventName);
@@ -79,6 +131,12 @@ public class EventManager {
 
     }
 
+    /**
+     * Adds a room to the list of rooms.
+     * @param roomNumber Refers to the room number.
+     * @param capacity Refers to the capacity of the room.
+     * @return Returns true if adding room was successful. Otherwise, returns false.
+     */
     public boolean addRoom(int roomNumber, int capacity){
 
         Room room = createNewRoom(roomNumber, capacity);
@@ -95,11 +153,24 @@ public class EventManager {
         return false;
     }
 
+
+    /**
+     * Creates a new room.
+     * @param roomNumber Refers to the room number.
+     * @param capacity Refers to the capacity of the room.
+     * @return Returns the room object.
+     */
     public Room createNewRoom(int roomNumber, int capacity){
         Room room = new Room(roomNumber, capacity);
         return room;
     }
 
+
+    /**
+     * Finds and gets the specified room from the list of rooms.
+     * @param roomNumber Refers to the room number.
+     * @return Returns the specified room.
+     */
     public Room getRoom(int roomNumber){
         for (Room room: rooms){
             if (room.getRoomNumber() == roomNumber){
