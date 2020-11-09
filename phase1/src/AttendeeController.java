4c438de5e2fb7678a4008eabad3fbbe9e7e87026
joshuaemerson <joshua.emerson@mail.utoronto.ca>
2020-11-08 20:14:33 -0500

    Implemented approximately half of the cases in the determineInput method within the AttendeeController class

--- phase1/src/AttendeeController.java
+++ phase1/src/AttendeeController.java
@@ -1,20 +1,74 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class AttendeeController extends MainController {
+    private Scanner scan = new Scanner(System.in);
 
-    void determineInput(String input) {}
+    public void run(){
+        String input = "";
+        input = scan.nextLine().toUpperCase();
+        while (!input.equals("END")){
+            determineInput(input);
+            input = scan.nextLine().toUpperCase();
+        }
+    }
 
-    void viewMessages(String username) {}
+    private void determineInput(String input) {
+        switch (input) {
+            case "See Inbox":
+                viewMessages(this.username);
+                break;
+            case "Send Message":
+                System.out.println("Who would you like to message?");
+                String recipient = scan.nextLine();
+                System.out.println("What message would you like to send to: " + recipient);
+                String messageContents = scan.nextLine();
+                sendMessages(recipient, messageContents);
+                break;
+            case "Reply To Message":
+                System.out.println("This is the oldest message in your inbox: '" +
+                        messageManager.viewMessages(this.username).get(messageManager.viewMessages(this.username).size()
+                                -  1) + "'. How would you like to respond?");
+                String response = scan.nextLine();
+                String responseUsername = messageManager.viewMessages(this.username).
+                        get(messageManager.viewMessages(this.username).size() -  1).getSender();
+                messageManager.viewMessages(this.username).remove(messageManager.viewMessages(this.username).
+                        size() -  1);
+                replyMessage(response, responseUsername);
+                break;
+            case "View Event List":
+                viewEventList(this.username);
+                break;
+            case "View My Scheduled Events":
 
-    void sendMessages(String recipient, String messageContents) {}
+                break;
+            case "Cancel Spot":
 
-    void replyMessage(String message, String originalMessanger) {};
+                break;
+            case "Sign Up For Event":
 
-    void viewEventList(String username) {}
+                break;
+            case "Add User To Contact List":
 
-    void viewSignedUpForEvent(String username) {}
+                break;
+        }
+    }
 
-    void cancelSpotInEvent(String eventName) {}
+    public void viewMessages(String username) {}
 
-    void signUp(String eventName) {}
+    public void sendMessages(String recipient, String messageContents) {}
 
-    void addUserToMessangeable(String username) {}
+    public void replyMessage(String message, String originalMessanger) {}
+
+    public void viewEventList(String username) {}
+
+    public void viewSignedUpForEvent(String username) {}
+
+    public void cancelSpotInEvent(String eventName) {}
+
+    public void signUp(String eventName) {}
+
+    public void addUserToMessageable(String username) {}
 }
