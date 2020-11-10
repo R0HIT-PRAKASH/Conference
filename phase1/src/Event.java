ab56c3b21b50461ed819cc8fb2bd587c070b0d9d
user2 <user2@guptas-MacBook-Air.local>
2020-11-10 10:08:29 -0800

    Neil -- Added Javadoc Comments

--- phase1/src/Event.java
+++ phase1/src/Event.java
@@ -11,6 +11,15 @@ public class Event {
     private int roomNumber;
     private Set<Attendee> attendeeSet;
 
+    /**
+     * This constructs an event
+     * @param name The name of the event
+     * @param speakerName The speaker at the event
+     * @param time The event time
+     * @param duration The event duration
+     * @param roomNumber The event room number
+     */
+
     public Event(String name, String speakerName, LocalDateTime time, int duration, int roomNumber) {
         this.name = name;
         this.speakerName = speakerName;
@@ -20,53 +29,99 @@ public class Event {
         attendeeSet = new HashSet<Attendee>();
     }
 
-    // Getters
+    /**
+     * This method is a getter for name
+     * @return String name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * This method is a getter for name
+     * @return String name
+     */
     public String getSpeakerName() {
         return speakerName;
     }
 
-
+    /**
+     * This method is a getter for event time
+     * @return LocalDateTime time
+     */
     public LocalDateTime getTime() {
         return time;
     }
 
+    /**
+     * This method is a getter for duration
+     * @return duration
+     */
     public int getDuration() {
         return duration;
     }
 
+    /**
+     * This method is a getter for roomNumber
+     * @return roomNumber
+     */
     public int getRoomNumber() {
         return roomNumber;
     }
 
+    /**
+     * This method is a getter for the set of attendees
+     * @return Set<Attendee> attendeeSet
+     */
     public Set<Attendee> getAttendeeSet() {
         return attendeeSet;
     }
 
-    // Setters
+
+    /**
+     * This method is a getter for the set of attendees
+     * @return Set<Attendee> attendeeSet
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * This method is a setter for the speaker
+     * @param speakerName
+     */
     public void setSpeakerName(String speakerName) {
         this.speakerName = speakerName;
     }
 
+    /**
+     * This method is a setter for the time
+     * @param time
+     */
     public void setTime(LocalDateTime time) {
         this.time = time;
     }
 
+    /**
+     * This method is a setter for the duration
+     * @param duration
+     */
     public void setDuration(int duration) {
         this.duration = duration;
     }
 
+    /**
+     * This method is a setter for the roomNumber
+     * @param roomNumber
+     */
     public void setRoomNumber(int roomNumber) {
         this.roomNumber = roomNumber;
     }
 
+    /**
+     * This method adds an attendee
+     * @param Attendee attendee
+     */
     public void addAttendee(Attendee attendee) {
         this.attendeeSet.add(attendee);
     }
--- phase1/src/OrganizerController.java
+++ phase1/src/OrganizerController.java
@@ -1,32 +1,64 @@
 import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.Map;
-import java.util.Scanner;
 
 public class OrganizerController extends AttendeeController {
 
-    public OrganizerController() {}
-
-    void addEvent(String name, String speaker, LocalDateTime time, int duration, int roomNumber) {
-        eventManager.createNewEvent(name, speaker, time, duration, roomNumber);
+    /**
+     * This method adds an event to the set of events in the conference
+     * @param name This parameter refers to the name of the event.
+     * @param address This parameter refers to the speaker at the event.
+     * @param email This parameter refers to the event time.
+     * @param username This parameter refers to the event duration.
+     * @param password This parameter refers to the room number.
+     * @return Returns true if the user was added to events map and false otherwise.
+     */
+    boolean addEvent(String name, String speaker, LocalDateTime time, int duration, int roomNumber) {
+        return eventManager.addEvent(name, speaker, time, duration, roomNumber);
     }
 
+    /**
+     * This method sends a message to all the attendees at the conference.
+     * @param message This parameter is the message text
+     * @return void
+     */
     void messageAllAttendees(String message) {
         createBlastMessage("attendee", message);
     }
 
+    /**
+     * This method sends a message to all the attendees at a particular event.
+     * @param message This parameter is the message text
+     * @param name This parameter is the event name
+     * @return void
+     */
     void messageEventAttendees(String message, String name) {
         messageManager.speakerBlastMessage(Arrays.asList(name), message, eventManager, this.username);
     }
 
+    /**
+     * This method sends a message to all the speakers at the conference.
+     * @param message This parameter is the message text
+     * @return void
+     */
     void messageAllSpeakers(String message) {
         createBlastMessage("speaker", message);
     }
 
+    /**
+     * This method cancels an event
+     * @param name This is the title of the event to be canceled
+     * @return void
+     */
     void cancelEvent(String name) {
         eventManager.getAllEvents().remove(name);
     }
 
+    /**
+     * This method reschedules an event
+     * @param name The name of the event
+     * @param newTime The new time
+     */
     void rescheduleEvent(String name, LocalDateTime newTime) {
 
         if(eventManager.getAllEvents().containsKey(name)) {
@@ -34,11 +66,25 @@ public class OrganizerController extends AttendeeController {
         }
     }
 
+    /**
+     * This method creates a speaker account
+     * @param name This parameter refers to the name of the speaker.
+     * @param address This parameter refers to the address of the speaker.
+     * @param email This parameter refers to the email of the speaker.
+     * @param username This parameter refers to the username of the speaker.
+     * @param password This parameter refers to the password of the speaker.
+     * @return Returns true if the user was added to userMap and false otherwise.
+     */
     void createSpeakerAccount(String name, String address, String email, String username, String password) {
         userManager.addUser(name, address, email, username, password, "speaker");
     }
 
 
+    /**
+     * This method sends messages to all people of a specific type
+     * @param blastType This is the type of the individual. Choices are "speaker", "organizer" and "attendee"
+     * @param message This is the actual message
+     */
     void createBlastMessage(String blastType, String message) {
 
         Map<String, String> userTypes = userManager.getUserTypes();
