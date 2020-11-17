4f09f48ad96e9214e64dac9c6edb161571ed04f4
ongjayde <jayden.ong@mail.utoronto.ca>
2020-11-17 17:42:44 -0500

    Added helper method helpSignUp() to optimize UserManager.

--- phase1/src/UserManager.java
+++ phase1/src/UserManager.java
@@ -175,14 +175,7 @@ public class UserManager {
 
         if(user.getUserType().equals("attendee")){
             for(String eventName : ((Attendee) user).getAttendingEvents()){
-                Event attendEvent = eventManager.getEvent(eventName);
-                double timeBetween = Math.abs(event.getTime().getHour()*60 + event.getTime().getMinute() -
-                        attendEvent.getTime().getHour()*60 - attendEvent.getTime().getMinute());
-                if(event.getTime().getDayOfYear() == attendEvent.getTime().getDayOfYear() &&
-                        event.getTime().getYear() == attendEvent.getTime().getYear() &&
-                        ((event.getTime().isBefore(attendEvent.getTime()) && timeBetween < event.getDuration()*60) ||
-                                (event.getTime().isAfter(attendEvent.getTime()) && timeBetween < attendEvent.getDuration()*60) ||
-                                (event.getTime().isEqual(attendEvent.getTime())))){
+                if(!helpSignUp(eventName, event, eventManager)){
                     return false;
                 }
             }
@@ -190,16 +183,7 @@ public class UserManager {
 
         }else if(user.getUserType().equals("organizer")){
             for(String eventName : ((Organizer) user).getAttendingEvents()){
-                Event attendEvent = eventManager.getEvent(eventName);
-                double timeBetween = Math.abs(event.getTime().getHour()*60 + event.getTime().getMinute() -
-                        attendEvent.getTime().getHour()*60 - attendEvent.getTime().getMinute());
-                if(event.getTime().getDayOfYear() == attendEvent.getTime().getDayOfYear() &&
-                        event.getTime().getYear() == attendEvent.getTime().getYear() &&
-                        ((event.getTime().isBefore(attendEvent.getTime()) && timeBetween < event.getDuration()*60) ||
-                                (event.getTime().isAfter(attendEvent.getTime()) && timeBetween < attendEvent.getDuration()*60) ||
-                                (event.getTime().isEqual(attendEvent.getTime())))){
-                    return false;
-                }
+                if(!helpSignUp(eventName, event, eventManager));
             }
             ((Organizer) user).signUpForEvent(event);
 
@@ -235,6 +219,27 @@ public class UserManager {
                 return false;
             }
         }else{
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * This is a helper method for signUpForEvent().
+     * @param eventName Refers to the name of the event in the list of attending events.
+     * @param event Refers to the event that might be added.
+     * @param eventManager Refers to the class that contains the list of events.
+     * @return Returns true if the events don't overlap and false otherwise.
+     */
+    public boolean helpSignUp(String eventName, Event event, EventManager eventManager){
+        Event attendEvent = eventManager.getEvent(eventName);
+        double timeBetween = Math.abs(event.getTime().getHour()*60 + event.getTime().getMinute() -
+                attendEvent.getTime().getHour()*60 - attendEvent.getTime().getMinute());
+        if(event.getTime().getDayOfYear() == attendEvent.getTime().getDayOfYear() &&
+                event.getTime().getYear() == attendEvent.getTime().getYear() &&
+                ((event.getTime().isBefore(attendEvent.getTime()) && timeBetween < event.getDuration()*60) ||
+                        (event.getTime().isAfter(attendEvent.getTime()) && timeBetween < attendEvent.getDuration()*60) ||
+                        (event.getTime().isEqual(attendEvent.getTime())))){
             return false;
         }
         return true;
