d8994d239952b4679c27d994571deaa75a4f130f
Khushkaran Rajput <k.rajput@mail.utoronto.ca>
2020-11-18 17:21:10 -0500

    LoginController: fixed an error with the Javadoc

--- phase1/src/LoginController.java
+++ phase1/src/LoginController.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
 
-/**
- * A controller that deals with logging into the program.
- */
 public class LoginController {
+    /**
+     * A controller that deals with logging into the program.
+     */
+
     private Scanner scan = new Scanner(System.in);
     private UserManager userManager;
     private MessageManager messageManager;
