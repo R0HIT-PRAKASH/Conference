a357f1eb7fc5e20615645a0d652aaa99e3b1fe94
Rohit Prakash <rohit.prakash@mail.utoronto.ca>
2020-11-19 22:11:06 -0500

    minor edit

--- phase1/src/Presenter.java
+++ phase1/src/Presenter.java
@@ -538,6 +538,7 @@ public class Presenter {
             System.out.println("No rooms have been created yet. ");
             return;
         }
+        System.out.println("These are all the created rooms");
         for(Room room : rooms){
             System.out.println("Room #" + room.getRoomNumber());
         }
