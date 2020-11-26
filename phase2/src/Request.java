200eeb4070760de2264d0e64c53de412fc98204f
Khushkaran Rajput <k.rajput@mail.utoronto.ca>
2020-11-26 14:32:50 -0500

    created new class Request, that represents a Request object

---
+++ phase2/src/Request.java
@@ -0,0 +1,8 @@
+import java.util.List;
+
+/**
+ * This class represents a Request object. Request objects have a string content, username of the requester, and an int
+ * requestStatus that contains the status of the request.
+ */
+public class Request {
+}
