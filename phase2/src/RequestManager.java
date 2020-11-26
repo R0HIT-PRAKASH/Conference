18b383c191708b815199d632f474ff0c6d8bd1b4
Khushkaran Rajput <k.rajput@mail.utoronto.ca>
2020-11-26 14:36:07 -0500

    created new class RequestManager, that handles Request objects

---
+++ phase2/src/RequestManager.java
@@ -0,0 +1,9 @@
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * The RequestManager class is responsible for handling request-related actions. allUserRequests is a map that maps
+ * usernames to all of their request objects.
+ */
+public class RequestManager {
+}
