687a694c362428b99efe92258650ee4264064fd9
1moonala <junseomoon9@gmail.com>
2020-11-28 18:01:37 -0500

    Skeleton of EventFactory

--- phase2/src/event/EventFactory.java
+++ phase2/src/event/EventFactory.java
@@ -1,4 +1,25 @@
 package event;
 
+
 public class EventFactory {
+
+    public Event getEvent(String eventType) {
+
+        if(eventType == null){
+            return null;
+        }
+
+        if(eventType.equalsIgnoreCase("Party")){
+            return new Party();
+        }
+
+        else if(eventType.equalsIgnoreCase("Talk")){
+            return new Talk();
+        }
+
+        else if(eventType.equalsIgnoreCase("Panel")){
+            return new Panel();
+        }
+
+    }
 }
