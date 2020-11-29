4c03b6a192ea53d6244c1bb46559fed0ba2cd572
Rohit Prakash <rohit.prakash@mail.utoronto.ca>
2020-11-29 15:16:09 -0500

    Renaming GUI.java

--- phase2/src/ConferenceGUI/ConferenceGUI.java
+++ phase2/src/ConferenceGUI/ConferenceGUI.java
@@ -1,16 +1,18 @@
-package GUI;
+package ConferenceGUI;
 
 import javax.swing.*;
 
-public class GUI {
+public class ConferenceGUI extends JFrame{
 
-    public Conference(){
+    private JPanel mainPanel;
 
+    public ConferenceGUI(String title){
+        super(title);
     }
 
     public static void main(String [] args){
         JFrame frame = new JFrame("Conference");
-        frame.setContentPane(new Conference().panelMain);
+        frame.setContentPane(new ConferenceGUI().panelMain);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
