7e4af496a261c5dc1f0a3c0a75ba3eb5d3041a85
Rohit Prakash <rohit.prakash@mail.utoronto.ca>
2020-11-29 15:10:58 -0500

    Creating GUI.java base

--- phase2/src/GUI/GUI.java
+++ phase2/src/GUI/GUI.java
@@ -1,4 +1,18 @@
 package GUI;
 
+import javax.swing.*;
+
 public class GUI {
+
+    public Conference(){
+
+    }
+
+    public static void main(String [] args){
+        JFrame frame = new JFrame("Conference");
+        frame.setContentPane(new Conference().panelMain);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
