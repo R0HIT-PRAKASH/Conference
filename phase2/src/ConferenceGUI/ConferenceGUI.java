821c63de6f5fc38782357ab2299ecc9278d43361
Rohit Prakash <rohit.prakash@mail.utoronto.ca>
2020-11-29 15:29:37 -0500

    First iteration

--- phase2/src/ConferenceGUI/ConferenceGUI.java
+++ phase2/src/ConferenceGUI/ConferenceGUI.java
@@ -1,16 +1,34 @@
 package ConferenceGUI;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class ConferenceGUI extends JFrame{
 
     private JPanel mainPanel;
+    private JTextField textField1;
+    private JPasswordField passwordField1;
+    private JButton loginButton;
+    private JButton createAccount;
 
     public ConferenceGUI(String title){
         super(title);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setContentPane(mainPanel);
         this.pack();
+        loginButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        createAccount.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
     }
 
     public static void main(String [] args){
